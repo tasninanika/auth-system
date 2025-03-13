842bdb3893789453b701c0a15cb83fc677358c85
anikatasnin <jarintasnin09@gmail.com>
2025-03-13 12:06:44 +0600

    added onsubmit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,24 +2,40 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="hero flex flex-col justify-center mt-20">
       <h2 className="text-4xl font-semibold mb-10">Login</h2>
       <div className="card bg-base-100 w-96 shadow-2xl">
         <div className="card-body">
-          <fieldset className="fieldset">
-            <label className="fieldset-label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
-            <label className="fieldset-label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
-            <div className="flex justify-between">
-              <a className="link link-hover">Forgot password?</a>
-              <NavLink to="register" className="pr-4">
-                Create an account
-              </NavLink>
-            </div>
-            <button className="btn btn-neutral mt-4">Login</button>
-          </fieldset>
+          <form>
+            <fieldset className="fieldset">
+              <label className="fieldset-label">Email</label>
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+              />
+              <label className="fieldset-label">Password</label>
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+              />
+              <div className="flex justify-between">
+                <a className="link link-hover">Forgot password?</a>
+                <NavLink to="register" className="pr-4">
+                  Create an account
+                </NavLink>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </form>
         </div>
       </div>
     </div>

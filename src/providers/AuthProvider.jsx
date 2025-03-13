434c2653c9807e2b5b9c2aff3be7703d885555c6
anikatasnin <jarintasnin09@gmail.com>
2025-03-13 13:08:25 +0600

    get user info using context

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,4 +1,6 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { createContext } from "react";
+import { auth } from "../firebase.init";
 
 export const AuthContext = createContext(null);
 
@@ -6,6 +8,11 @@ const AuthProvider = ({ children }) => {
   const authInfo = {
     name: "jarin",
   };
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );

2eb14c21d3fc19438c08e8891a2d60bdd8100bd2
anikatasnin <jarintasnin09@gmail.com>
2025-03-13 11:39:48 +0600

    added login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="hero flex flex-col justify-center mt-20">
+      <h2 className="text-4xl font-semibold mb-10">Login</h2>
+      <div className="card bg-base-100 w-96 shadow-2xl">
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <label className="fieldset-label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="fieldset-label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <div className="flex justify-between">
+              <a className="link link-hover">Forgot password?</a>
+              <NavLink to="register" className="pr-4">
+                Create an account
+              </NavLink>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </div>
+      </div>
     </div>
   );
 };

6eb4d69bb31c9ec6d5a094006e57f4e81fe2310f
anikatasnin <jarintasnin09@gmail.com>
2025-03-13 11:34:08 +0600

    added register form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,8 +2,27 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <h2>Register</h2>
+    <div className="hero flex flex-col justify-center mt-20">
+      <h2 className="text-4xl font-semibold mb-10">Register Now!</h2>
+      <div className="card bg-base-100 w-96 shadow-2xl">
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <label className="fieldset-label">Name</label>
+            <input type="text" className="input" placeholder="Name" />
+            <label className="fieldset-label">Photo URL</label>
+            <input type="text" className="input" placeholder="URL" />
+            <label className="fieldset-label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="fieldset-label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <label className="fieldset-label mt-2">
+              <input type="checkbox" defaultChecked className="checkbox" />
+              Accepts Our Terms & Conditions
+            </label>
+            <button className="btn btn-neutral mt-4">Register</button>
+          </fieldset>
+        </div>
+      </div>
     </div>
   );
 };
